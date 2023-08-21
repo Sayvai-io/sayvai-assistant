dfd2c49567be6d7d37524dd5126f8b4685e9b81d
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-21 22:11:35 +0530

    init agent

--- assistant.py
+++ assistant.py
@@ -2,6 +2,37 @@
 """
 1) Tools
  1.1) Pinecone
- 1.2) 
- 
+ 1.2) human
+ 1.3) Sql
+ 1.4) calendly
+2) Agent
+3) Memory 
+ 3.1) summarization
 """
+import os
+from langchain.llms import OpenAI 
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import HumanInputRun as human
+from langchain.agents import AgentType, Tool, AgentExecutor , initialize_agent
+from langchain.schema.messages import SystemMessage
+
+
+class Assistant:
+    """
+    The assistant is a class that is used to interact with the user and the agent. 
+    It is the main interface for the user to interact with the agent."""
+    def __init__(self):
+        self.agent = None
+        self.memory = None
+        self.tools = None
+        self.human = None
+        self.sql = None
+        self.calendly = None
+        self.vectors = None
+    
+        
+    def initialize(self):
+        """Initialize the assistant"""
+        
+
+        
