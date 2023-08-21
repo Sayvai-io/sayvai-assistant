9aed6aa650c5c5ca8057a7247e4355bc971dbbee
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-21 22:43:17 +0530

    init vector db

--- assistant.py
+++ assistant.py
@@ -10,12 +10,33 @@
  3.1) summarization
 """
 import os
+import pinecone
+from dbbase import SQLDatabase
+from dbchain import SQLDatabaseChain
 from langchain.llms import OpenAI 
 from langchain.chat_models import ChatOpenAI
 from langchain.tools import HumanInputRun as human
-from langchain.agents import AgentType, Tool, AgentExecutor , initialize_agent
+from langchain.agents import AgentType, Tool, AgentExecutor , initialize_agent , OpenAIFunctionsAgent
 from langchain.schema.messages import SystemMessage
+from sqlalchemy import create_engine
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.prompts.prompt import PromptTemplate
 
+
+
+with open("openai_api_key.txt", "r") as f:
+    api_key = f.read()
+
+with open("pinecone_api.txt", "r") as f:
+    pinecone_api_key = f.read()
+    
+
+os.environ["OPENAI_API_KEY"] = api_key
+
+llm = ChatOpenAI(
+    temperature=0.4,
+)
 
 class Assistant:
     """
@@ -29,10 +50,29 @@ class Assistant:
         self.sql = None
         self.calendly = None
         self.vectors = None
+        self.index = "index-1"
     
+    
+    def initialize_vectordb(self):
+        """Initialize the vector database"""
+        self.vectors = pinecone.init(
+            api_key=pinecone_api_key,
+            environment="northamerica-northeast1-gcp"
+        )
+        pass 
+        
+    def intialize_tools(self):
+        """Initialize the tools"""
+        if self.tools is None:
+            self.tools = Tool()
+        else :
+            print("Tools already initialized")
         
     def initialize(self):
         """Initialize the assistant"""
+        
+    
+        
         
 
         
