02c1c78d332f0c99467d9af944ca40bf2e683c19
Kedareeshwar Sekar <kedareshwar72381@gmail.com>
2023-08-22 22:59:43 +0530

    added calender.py

--- constants.py
+++ constants.py
@@ -3,3 +3,5 @@
 # from langchain.schema.messages import SystemMessage
 
 # agent_prompt = SystemMessage(content="You are assistant that works for sayvai.Interacrt with user untill he opt to exit")
+
+SCOPES = 'https://www.googleapis.com/auth/calendar'
