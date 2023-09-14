4ee6fee818ff9ddf6b549c0114a064670fa1e3f9
Sanjaypranav <sanjaypriya195@gmail.com>
2023-09-14 12:39:44 +0530

    updated

--- constants.py
+++ constants.py
@@ -1,9 +1,12 @@
 # constants file
 
 from langchain.schema.messages import SystemMessage
+from langchain.prompts.prompt import PromptTemplate
 
 prompt = SystemMessage(content="""
-                       You are Sayvai, a virtual assistant. All the output from the llm should be sent to human tool.""")
+                       You are Sayvai, a virtual assistant. All the output from the llm should be sent to human tool.
+                       Access sql if you need details like mail, mobile and desiignation of employees for scheduling meet.
+                       """)
 
 # from langchain.prompts.prompt import PromptTemplate
 # from langchain.schema.messages import SystemMessage
@@ -12,3 +15,20 @@ prompt = SystemMessage(content="""
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 
+
+PROMPT_SUFFIX = """Only use the following tables:
+{table_info}
+
+Question: {input}"""
+
+_DEFAULT_TEMPLATE = """
+You are a sayvai assistant . When given a question, you need to create a valid SQL query in the specified {dialect} to select table user.
+
+SQLQuery: query to select table user
+Answer: Provide results from SQLQuery.
+"""
+
+PROMPT = PromptTemplate(
+    input_variables=["input", "table_info", "dialect"],
+    template=_DEFAULT_TEMPLATE + PROMPT_SUFFIX,
+)
---
+++ tools/date.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+def current_date(cur_date: str):
+    """Get the current date"""
+    return datetime.now()
